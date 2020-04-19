e15bf6251f418ae7d39dbd3813a8c441179ea7ae
Crowdbotics <team@crowdbotics.com>
2020-04-19 20:00:10 +0000

    Update models

--- music_paradise_even_16027/wsgi.py
+++ music_paradise_even_16027/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_paradise_even_16027.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "music_paradise_even_16027.settings")
 
 application = get_wsgi_application()
